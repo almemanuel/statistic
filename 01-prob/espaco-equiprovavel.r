c9b2a0a3ce782cbcaa8ca4ad3d197f92e9c50b2b
almemanuel <core.editor>
2021-11-23 09:27:50 -0300

    esp

---
+++ 01-prob/espaco-equiprovavel.r
@@ -0,0 +1,11 @@
+# Lancamento de um dado
+S <- 1:6 # Espaço amostral
+E <- c(2, 4, 6) # Evento: sair numero par
+
+length(E) / length(S) # Probabilidade de sair um numero par
+
+E <- 3 # Evento: sair 3
+length(E) / length(S) # Probabilidade sair 3
+
+E <- 1:2 # probabilidade de sair um numero menor que 3
+length(E) / length(S)
